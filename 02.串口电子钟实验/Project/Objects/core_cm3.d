ce2ef3dd3fef685e64d23b24ed6c515d237e6a74
yangyy-ming <1836228787@qq.com>
2025-08-07 21:54:56 +0800

    修改并实现了每秒更新日期

---
+++ 02.串口电子钟实验/Project/Objects/core_cm3.d
@@ -0,0 +1,2 @@
+.\objects\core_cm3.o: ..\ARM\System\core_cm3.c
+.\objects\core_cm3.o: C:\Users\18362\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
---
+++ 02.串口电子钟实验/Project/Objects/runclock.d
@@ -0,0 +1,4 @@
+.\objects\runclock.o: ..\App\RunClock\RunClock.c
+.\objects\runclock.o: ..\App\RunClock\RunClock.h
+.\objects\runclock.o: ..\App\DataType\DataType.h
+.\objects\runclock.o: C:\Users\18362\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
